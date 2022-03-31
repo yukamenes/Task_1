df3405b5ecbc75e479ab01cae72064fc1bb2084f
Yulia Menes <menesj@ukr.net>
2022-03-31 13:23:32 +0300

    First task

---
+++ Task_1/EnterName/EnterName/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using EnterName.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnterName.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*public IActionResult Privacy()
+        {
+            return View();
+        }*/
+
+        [HttpPost]
+        public IActionResult Index(string name)
+        {
+            ViewData["Message"] = "Hello, " + name + "!";
+            return View("Result");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
