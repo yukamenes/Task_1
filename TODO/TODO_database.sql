ffcef5579e6c55cda4cb192531165b3f78cff95a
Yulia Menes <menesj@ukr.net>
2022-04-18 15:18:47 +0300

    TODO_DB

---
+++ TODO/TODO_database.sql
@@ -0,0 +1,43 @@
+create database TODO /* створення БД */
+
+use TODO /*передання назви БД, в контексті якої необхідно виконати подальший скрипт*/
+
+/*створення таблиці "Категорії"*/
+/*id категорії - зайжди має унікальне значення, IDENTITY - перший рядок - 1, кожен наступний - i+1,
+name_category - має тип char та буде містити ім'я категорії, що немає обмеження в довжині,
+id_task - містить id завдання, що входить в дану категорію*/
+create table category
+(
+id_category int IDENTITY(1,1) primary key,
+name_category nvarchar(MAX),
+description_category nvarchar(MAX)
+)
+
+/*створення таблиці "Завдання"*/
+/*id завдання - зайжди має унікальне значення, IDENTITY - перший рядок - 1, кожен наступний - i+1
+name - має тип char та буде містити ім'я завдання, що немає обмеження в довжині,
+complete - має тип char та бути містити інформацію про те, чи завдання виконанане/невиконане, має довжину в 3 символи
+not null check - поле завжди містить значення*/
+
+create table tasks
+(id_task int IDENTITY(1,1) primary key, 
+name_task nvarchar(MAX),
+complete char(3) not null check(complete in ('Так','Ні')),
+id_category int not null foreign key (id_category) references dbo.category(id_category) on delete no action on update no action
+)
+
+select * from category
+insert into category values
+('Дім', 'Робота по дому')
+
+select * from tasks
+insert into tasks values
+('Попити чай', 'Так','1')
+
+
+select tasks.name_task, category.name_category
+from category
+join tasks on category.id_category = tasks.id_task
+
+drop database TODO
+
